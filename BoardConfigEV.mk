446f99811d4ba3dc942a4208681a59890db28078
Nick Reuter <nreuter85@gmail.com>
2014-04-18 00:15:06 -0500

    hammerhead: use evervolv config for kernel
    Change-Id: I14830b1be9c8c093c6fc7e2252c9bbb33a22bdf4

--- BoardConfigEV.mk
+++ BoardConfigEV.mk
@@ -5,7 +5,7 @@ TARGET_KERNEL_NO_MODULES := true
 BUILD_KERNEL := true
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
-TARGET_KERNEL_CONFIG := hammerhead_defconfig
+TARGET_KERNEL_CONFIG := evervolv_hammerhead_defconfig
 
 #TWRP
 DEVICE_RESOLUTION := 1080x1920
