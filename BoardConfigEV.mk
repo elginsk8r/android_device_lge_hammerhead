27abb9a7d63ee78cb17ed2ea8ec57089177b3942
Nick Reuter <nreuter85@gmail.com>
2014-04-22 01:17:52 -0500

    Revert "HACK: Disable secure discard"
    This reverts commit 499335d644d72560f72a3ed912434deb0322c4f3.
    
    Change-Id: I092be33a22bfeb55b0a0ffbcbd3a3bf896700b58

--- BoardConfigEV.mk
+++ BoardConfigEV.mk
@@ -6,6 +6,3 @@ BUILD_KERNEL := true
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 TARGET_KERNEL_CONFIG := evervolv_hammerhead_defconfig
-
-# Flags
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
