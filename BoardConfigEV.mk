e99ebb4ea2caa5821b710707e92335be22c3fe8e
Nick Reuter <nreuter85@gmail.com>
2014-04-22 01:18:49 -0500

    hammerhead: enable BOARD_SUPPRESS_SECURE_ERASE
     * with this enabled it greatly reduces formatting times
    
    Change-Id: I58db88456ebb39a83923ba5c167434298bf85760

--- BoardConfigEV.mk
+++ BoardConfigEV.mk
@@ -6,3 +6,6 @@ BUILD_KERNEL := true
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 TARGET_KERNEL_CONFIG := evervolv_hammerhead_defconfig
+
+# Flags
+BOARD_SUPPRESS_SECURE_ERASE := true
