5ddd86020fa5101679e563077de1fa58a6e916ce
Chirayu Desai <chirayudesai1@gmail.com>
2016-08-27 17:07:14 +0530

    Use GCC 4.8 for kernel
    * 4.9 is the N / CM14 default, current kernel
      doesn't compile with it.
    
    TODO: Fix the kernel
    
    Change-Id: Ief997d1a64cbc0a0e7e6200989c29c9ca7197e39

--- BoardConfigEV.mk
+++ BoardConfigEV.mk
@@ -6,3 +6,5 @@ BUILD_KERNEL := true
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 TARGET_KERNEL_CONFIG := evervolv_hammerhead_defconfig
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
