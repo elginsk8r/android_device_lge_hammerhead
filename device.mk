KangDroid/android_device_moto_shamu

Added light HAL
Bug: 32022100
Test: pass
Change-Id: Ib4b37a86fc278d7ff94d65885ea999f4fd409085

device.mk
@@ -199,7 +199,8 @@ PRODUCT_PACKAGES += \
     libion
 
 PRODUCT_PACKAGES += \
-    lights.shamu
+    lights.shamu \
+    android.hardware.light@2.0-impl
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \

elginsk8r/android_device_lge_hammerhead

device.mk
@@ -252,7 +252,8 @@ PRODUCT_PACKAGES += \
     libion
 
 PRODUCT_PACKAGES += \
-    lights.hammerhead
+    lights.hammerhead \
+    android.hardware.light@2.0-impl
 
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory