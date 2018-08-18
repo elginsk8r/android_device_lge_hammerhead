6c56b42e82d26bc8b916d8eb3e7cfc6f8838f8fa
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-08-18 13:46:21 +0300

    board: Add an M4 alias for (vendor|system/vendor)
    Change-Id: I58ba5d9957b0ebdd9f48e390afdda59a2baf01da
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,6 +122,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/lge/hammerhead
 BOARD_HAL_STATIC_LIBRARIES := libdumpstate.hammerhead
 
 BOARD_SEPOLICY_DIRS += device/lge/hammerhead/sepolicy
+BOARD_SEPOLICY_M4DEFS += vensys=\(vendor\|system/vendor\)
 
 ifneq ($(filter hammerhead_fp aosp_hammerhead_fp,$(TARGET_PRODUCT)),)
 BOARD_SEPOLICY_DIRS += \
